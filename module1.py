5bf37bee88087f60a6dc8e26967b1b2dac65feff
ramaouta <masofarel@yahoo.fr>
2022-06-01 15:37:23 +0200

    first commit

---
+++ module1.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrateur
+#
+# Created:     01/06/2022
+# Copyright:   (c) Administrateur 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+str = 'helloworld'
